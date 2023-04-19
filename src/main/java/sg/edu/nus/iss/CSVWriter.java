38d4ea36ccaaf98ae3c368215bcc0b204d41a2d0
jylmarcus <maplecow@gmail.com>
2023-04-19 15:37:01 +0800

    Added CSVReader, CSVWriter, Employee

---
+++ src/main/java/sg/edu/nus/iss/CSVWriter.java
@@ -0,0 +1,55 @@
+package sg.edu.nus.iss;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class CSVWriter {
+    public static final String COMMA_DELIMITER = ",";
+    public static final String NEWLINE_SEPARATOR = "\n";
+    public static final String FILE_HEADER = "staffNo,fullName,department,role,emailAddress,salary";
+
+
+    public List<Employee> employees = null;
+
+    public List<Employee> generateEmployees() {
+        employees = new ArrayList<>();
+
+        Employee emp1 = new Employee("12344", "Bryan", "Customer Relation", "Relationship Manager", "bryan@visa.com", 10000);
+        Employee emp2 = new Employee("12345", "Lee", "Human Resources", "Hiring Manager", "lee@visa.com", 11000);
+        Employee emp3 = new Employee("12346", "Bryant", "Technical Support", "Support Specialist", "bryant@visa.com", 12000);
+
+        employees.add(emp1);
+        employees.add(emp2);
+        employees.add(emp3);
+
+        return employees;
+    }
+
+    public void writeToCSV(List<Employee> employees, String fileName) throws IOException {
+        FileWriter fileWriter = new FileWriter(fileName);
+        
+        //write the FILE_HEADER
+        fileWriter.append(FILE_HEADER.toString());
+        fileWriter.append(NEWLINE_SEPARATOR);
+        //iterate through employees to write each employee record to file
+        for(Employee emp: employees) {
+            fileWriter.append(emp.getStaffNo());
+            fileWriter.append(COMMA_DELIMITER);
+            fileWriter.append(emp.getFullName());
+            fileWriter.append(COMMA_DELIMITER);
+            fileWriter.append(emp.getDepartment());
+            fileWriter.append(COMMA_DELIMITER);
+            fileWriter.append(emp.getRole());
+            fileWriter.append(COMMA_DELIMITER);
+            fileWriter.append(emp.getEmailAddress());
+            fileWriter.append(COMMA_DELIMITER);
+            fileWriter.append(String.valueOf(emp.getSalary()));
+            fileWriter.append(NEWLINE_SEPARATOR);
+
+        }
+        //flush and close FileWriter
+        fileWriter.flush();
+        fileWriter.close();
+    }  
+}
